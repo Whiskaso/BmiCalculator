4a88f5e479d0f1df79c3e1152bcbbfbed13ade1d
Whiskaso <marcinlakomy9@gmail.com>
2020-03-30 20:35:17 +0200

    add styling

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function calculatingBMI(){
 
 
     //INSERT DATA TO HTML DOCUMENT
-    document.querySelector('h2').innerHTML = `Your BMI is ${bmi.toFixed(1)}.`
+    document.querySelector('h2').innerHTML = `${bmi.toFixed(1)}`
     document.querySelector('h3').innerHTML = `You weight is: ${weightCategory}.`
     
     document.getElementById('printBmiCategories').innerHTML =  `BMI Categories:<br>Underweight = <18.5<br>Normal weight = 18.5–24.9<br>Overweight = 25–29.9<br>Obesity = BMI of 30 or greater`
